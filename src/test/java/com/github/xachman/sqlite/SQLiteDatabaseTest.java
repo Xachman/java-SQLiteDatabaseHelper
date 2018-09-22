85e3661bc23543e1d03cab856ac073c0a08acacc
xachman <zironside@hotmail.com>
2018-09-22 07:50:01 -0400

    refactor test

--- src/test/java/com/github/xachman/sqlite/SQLiteDatabaseTest.java
+++ src/test/java/com/github/xachman/sqlite/SQLiteDatabaseTest.java
@@ -2,12 +2,7 @@ package com.github.xachman.sqlite;
 
 import com.github.xachman.Row;
 import com.github.xachman.Table;
-import com.github.xachman.sqlite.SQLiteDatabaseConnection;
-import com.github.xachman.sqlite.SQLiteDatabase;
 import com.github.xachman.mocks.TestTableMock;
-import com.github.xachman.Where.Comparison;
-import com.github.xachman.Where.Condition;
-import com.github.xachman.Where.Where;
 import com.github.xachman.mocks.UsersAnnotation;
 import org.junit.After;
 import org.junit.Assert;
@@ -17,7 +12,6 @@ import org.junit.Test;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.lang.reflect.Array;
 import java.util.*;
 
 /**
@@ -43,12 +37,12 @@ public class SQLiteDatabaseTest {
             List<Row> rows = dbc.executeQuery("SELECT name FROM sqlite_master WHERE type='table'");
             Row row = rows.get(0);
 
-            Assert.assertEquals(testTable.tableName().toString(), row.getEntry(0).getValue().toString());
+            Assert.assertEquals(testTable.tableName(), row.getEntry(0).getValue().toString());
 
             List<Row> pRows = dbc.executeQuery("PRAGMA table_info(users)");
             int count = 0;
             for(Row pRow: pRows) {
-                Assert.assertEquals(pRow.getEntry(2).getValue().toString(), testTable.columns().get(count).type().toString());
+                Assert.assertEquals(pRow.getEntry(2).getValue().toString(), testTable.columns().get(count).type());
                 count++;
             }
         } catch (FileNotFoundException e) {
@@ -118,12 +112,12 @@ public class SQLiteDatabaseTest {
         Table testTable = new TestTableMock();
         dbh.createTable(testTable);
 
-        Map<String, String> map1 = new HashMap<String, String>();
+        Map<String, String> map1 = new HashMap<>();
         map1.put("first_name", "test1");
         map1.put("last_name", "test2");
         map1.put("age", "23");
 
-        Map<String, String> map2 = new HashMap<String, String>();
+        Map<String, String> map2 = new HashMap<>();
         map2.put("first_name", "row2test1");
         map2.put("last_name", "row2test2");
         map2.put("age", "27");
@@ -133,17 +127,17 @@ public class SQLiteDatabaseTest {
 
         Row row = dbh.insert(testTable, map1);
 
-        Assert.assertEquals(row.getEntry(0).getValue().toString(), "1");
-        Assert.assertEquals(row.getEntry(1).getValue().toString(), "test1");
-        Assert.assertEquals(row.getEntry(2).getValue().toString(), "test2");
-        Assert.assertEquals(row.getEntry(3).getValue().toString(), "23");
+        Assert.assertEquals("1", row.getEntry(0).getValue().toString());
+        Assert.assertEquals("test1", row.getEntry(1).getValue().toString());
+        Assert.assertEquals("test2", row.getEntry(2).getValue().toString());
+        Assert.assertEquals("23", row.getEntry(3).getValue().toString());
 
         Row row2 = dbh.insert(testTable, map2);
 
-        Assert.assertEquals(row2.getEntry(0).getValue().toString(), "2");
-        Assert.assertEquals(row2.getEntry(1).getValue().toString(), "row2test1");
-        Assert.assertEquals(row2.getEntry(2).getValue().toString(), "row2test2");
-        Assert.assertEquals(row2.getEntry(3).getValue().toString(), "27");
+        Assert.assertEquals("2", row2.getEntry(0).getValue().toString());
+        Assert.assertEquals("row2test1", row2.getEntry(1).getValue().toString());
+        Assert.assertEquals("row2test2", row2.getEntry(2).getValue().toString());
+        Assert.assertEquals("27", row2.getEntry(3).getValue().toString());
     }
 
     @Test
@@ -155,9 +149,9 @@ public class SQLiteDatabaseTest {
 
         List<Row> rows = dbh.getRows(testTable);
 
-        Assert.assertEquals(rows.get(0).getEntry(1).getValue().toString(), "test1");
-        Assert.assertEquals(rows.get(1).getEntry(1).getValue().toString(), "test3");
-        Assert.assertEquals(rows.get(2).getEntry(1).getValue().toString(), "test5");
+        Assert.assertEquals("test1", rows.get(0).getEntry(1).getValue().toString());
+        Assert.assertEquals("test3", rows.get(1).getEntry(1).getValue().toString());
+        Assert.assertEquals("test5", rows.get(2).getEntry(1).getValue().toString());
     }
 
     @Test
@@ -169,7 +163,7 @@ public class SQLiteDatabaseTest {
 
         Row row = dbh.getRowById(testTable, 2);
 
-        Assert.assertEquals(row.getEntry(1).getValue().toString(), "test3");
+        Assert.assertEquals("test3", row.getEntry(1).getValue().toString());
     }
 
     @Test
@@ -179,17 +173,17 @@ public class SQLiteDatabaseTest {
 
         createInsert(dbh, testTable);
 
-        Map<String, String> map = new HashMap<String, String>();
+        Map<String, String> map = new HashMap<>();
         map.put("first_name", "test7");
         map.put("last_name", "test8");
         map.put("age", "30");
 
         Row row = dbh.updateById(testTable,2, map);
 
-        Assert.assertEquals(row.getEntry(0).getValue().toString(), "2");
-        Assert.assertEquals(row.getEntry(1).getValue().toString(), "test7");
-        Assert.assertEquals(row.getEntry(2).getValue().toString(), "test8");
-        Assert.assertEquals(row.getEntry(3).getValue().toString(), "30");
+        Assert.assertEquals("2", row.getEntry(0).getValue().toString());
+        Assert.assertEquals("test7", row.getEntry(1).getValue().toString());
+        Assert.assertEquals("test8", row.getEntry(2).getValue().toString());
+        Assert.assertEquals("30", row.getEntry(3).getValue().toString());
     }
 
     @Test
@@ -205,17 +199,17 @@ public class SQLiteDatabaseTest {
 
         dbh.close();
 
-        Assert.assertEquals(rows.get(0).getEntry(0).getValue().toString(), "2");
-        Assert.assertEquals(rows.get(0).getEntry(1).getValue().toString(), "test3");
-        Assert.assertEquals(rows.get(0).getEntry(2).getValue().toString(), "test4");
-        Assert.assertEquals(rows.get(0).getEntry(3).getValue().toString(), "27");
+        Assert.assertEquals("2", rows.get(0).getEntry(0).getValue().toString());
+        Assert.assertEquals("test3", rows.get(0).getEntry(1).getValue().toString());
+        Assert.assertEquals("test4", rows.get(0).getEntry(2).getValue().toString());
+        Assert.assertEquals("27", rows.get(0).getEntry(3).getValue().toString());
 
-        Assert.assertEquals(rows.get(1).getEntry(0).getValue().toString(), "3");
-        Assert.assertEquals(rows.get(1).getEntry(1).getValue().toString(), "test5");
-        Assert.assertEquals(rows.get(1).getEntry(2).getValue().toString(), "test6");
-        Assert.assertEquals(rows.get(1).getEntry(3).getValue().toString(), "28");
+        Assert.assertEquals("3", rows.get(1).getEntry(0).getValue().toString());
+        Assert.assertEquals("test5", rows.get(1).getEntry(1).getValue().toString());
+        Assert.assertEquals("test6", rows.get(1).getEntry(2).getValue().toString());
+        Assert.assertEquals("28", rows.get(1).getEntry(3).getValue().toString());
 
-        Assert.assertEquals(rows.size(), 2);
+        Assert.assertEquals(2, rows.size());
     }
 
     @Test
@@ -233,10 +227,10 @@ public class SQLiteDatabaseTest {
 
         dbh.close();
 
-        Assert.assertEquals(rows.get(0).getEntry(0).getValue().toString(), "2");
-        Assert.assertEquals(rows.get(0).getEntry(1).getValue().toString(), "test3");
-        Assert.assertEquals(rows.get(0).getEntry(2).getValue().toString(), "test4");
-        Assert.assertEquals(rows.get(0).getEntry(3).getValue().toString(), "27");
+        Assert.assertEquals("2", rows.get(0).getEntry(0).getValue().toString());
+        Assert.assertEquals("test3", rows.get(0).getEntry(1).getValue().toString());
+        Assert.assertEquals("test4", rows.get(0).getEntry(2).getValue().toString());
+        Assert.assertEquals("27", rows.get(0).getEntry(3).getValue().toString());
 
     }
     @After
@@ -252,18 +246,18 @@ public class SQLiteDatabaseTest {
     }
 
     private void createInsert(SQLiteDatabase dbh, Table testTable) {
-        Map<String, String> map1 = new HashMap<String, String>();
+        Map<String, String> map1 = new HashMap<>();
         map1.put("first_name", "test1");
         map1.put("last_name", "test2");
         map1.put("age", "26");
 
 
-        Map<String, String> map2 = new HashMap<String, String>();
+        Map<String, String> map2 = new HashMap<>();
         map2.put("first_name", "test3");
         map2.put("last_name", "test4");
         map2.put("age", "27");
 
-        Map<String, String> map3 = new HashMap<String, String>();
+        Map<String, String> map3 = new HashMap<>();
         map3.put("first_name", "test5");
         map3.put("last_name", "test6");
         map3.put("age", "28");
